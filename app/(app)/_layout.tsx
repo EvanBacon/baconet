23dca9e5cb2b11d2fa9d6dbf2e319f000e5f2d48
evanbacon <bacon@expo.io>
2023-02-07 12:00:14 -0600

    idk

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,26 +1,19 @@
 import { Head } from "@bacons/head";
-import { Image, View } from "@bacons/react-views";
+import { View } from "@bacons/react-views";
 import {
   CommonActions,
   NavigationHelpersContext,
 } from "@react-navigation/native";
-import { TabRouter } from "@react-navigation/routers";
-import {
-  Link,
-  Navigator,
-  Slot,
-  // Tabs
-} from "expo-router";
+import { Link, Navigator } from "expo-router";
+import { QualifiedSlot, Slot } from "expo-router/build/views/Layout";
 import React from "react";
 import { useWindowDimensions } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import * as Icons from "../../components/medium";
+import { Tabs } from "../../components/tab-bar/BottomTabs";
 import { makeIcon } from "../../components/TabBarIcon";
 
 function HeaderLogo() {
-  const isLarge = useWidth(1265);
-
   return (
     <Link style={{ paddingVertical: 40 }} href="/" replace>
       <h1
@@ -46,36 +39,14 @@ function useWidth(size) {
   return width >= size;
 }
 
-function VerticalTabBar({ children, style }) {
-  const childrenArray = React.Children.toArray(children);
-
-  const newChildren = childrenArray.map((child, index) => {
-    return (
-      <View
-        key={String(index)}
-        asChild
-        style={{ display: "flex", paddingBottom: 35 }}
-      >
-        {child}
-      </View>
-    );
-  });
-
-  return <nav style={style}>{newChildren}</nav>;
-}
-
 function SideBar() {
   const isLarge = useWidth(1265);
-  // const isMedSideBar = useWidth(600);
 
   const navigation = React.useContext(NavigationHelpersContext)!;
 
   const state = navigation.getState();
-  const current = state.routes.find((route, i) => {
-    return state.index === i;
-  });
+  const current = state.routes.find((route, i) => state.index === i);
 
-  console.log("current", current);
   return (
     <div
       style={[
@@ -128,7 +99,7 @@ function SideBar() {
         >
           <HeaderLogo />
 
-          <VerticalTabBar style={{}}>
+          <nav>
             <SideBarTabItem
               name="index"
               selected={current.name === "(index)"}
@@ -137,50 +108,24 @@ function SideBar() {
             >
               Home
             </SideBarTabItem>
-            {false && (
-              <SideBarTabItem
-                href="/lego"
-                selected={current.name === "(lego)"}
-                icon={makeIcon("build")}
-              >
-                Lego
-              </SideBarTabItem>
-            )}
-            {false && (
-              <SideBarTabItem
-                href="/games"
-                selected={current.name === "(games)"}
-                icon={makeIcon("game-controller")}
-              >
-                Games
-              </SideBarTabItem>
-            )}
             <SideBarTabItem
+              name="(media)"
               href="/media"
               selected={current.name === "(media)"}
               icon={makeIcon("mic")}
             >
               Media
             </SideBarTabItem>
-            <div>
-              <div
-                style={{
-                  marginHorizontal: isLarge ? 0 : "auto",
-                  width: isLarge ? "80%" : "33%",
-                  backgroundColor: "rgba(230, 230, 230, 1)",
-                  maxHeight: 1,
-                  height: 1,
-                }}
-              />
-            </div>
+            <SideBarDivider />
             <SideBarTabItem
+              name="about"
               href="/about"
               selected={current.name === "(about)"}
               icon={makeIcon("person")}
             >
               Media
             </SideBarTabItem>
-          </VerticalTabBar>
+          </nav>
 
           <footer
             style={{
@@ -205,20 +150,65 @@ function SideBar() {
   );
 }
 
-function TabLink({ focused, name, ...props }) {
-  const { state, navigation } = Navigator.useContext();
-  const route = state.routes.find((route, i) => {
+function SideBarDivider() {
+  const isLarge = useWidth(1265);
+
+  return (
+    <div>
+      <div
+        style={{
+          marginHorizontal: isLarge ? 0 : "auto",
+          width: isLarge ? "80%" : "33%",
+          backgroundColor: "rgba(230, 230, 230, 1)",
+          maxHeight: 1,
+          height: 1,
+        }}
+      />
+    </div>
+  );
+}
+
+function useContextRoute(name: string) {
+  const context = Navigator.useContext();
+
+  const { state, descriptors } = context;
+
+  const current = state.routes.find((route, i) => {
     return route.name === name;
   });
 
-  if (!route) {
-    throw new Error(
+  if (!current) {
+    console.warn(
       `Could not find route with name: ${name}. Options: ${state.routes
         .map((r) => r.name)
         .join(", ")}`
     );
   }
 
+  // const current = state.routes.find((route, i) => {
+  //   return state.index === i;
+  // });
+
+  if (!current) {
+    return null;
+  }
+
+  return {
+    route: current,
+    descriptor: descriptors[current.key],
+  };
+}
+
+function TabLink({ focused, name, ...props }) {
+  const { state, navigation } = Navigator.useContext();
+  const ctxRoute = useContextRoute(name);
+  console.log("sate:", name, ctxRoute);
+
+  if (!ctxRoute) {
+    return null;
+  }
+
+  const { route } = ctxRoute;
   // const navigation = useNavigation();
 
   const onPress = () => {
@@ -246,18 +236,31 @@ function TabLink({ focused, name, ...props }) {
   return <Link onPress={onPress} onLongPress={onLongPress} {...props} />;
 }
 
-function SideBarTabItem({ children, href, icon, selected, name }) {
+function SideBarTabItem({
+  children,
+  href,
+  icon,
+  selected,
+  name,
+}: {
+  children: string;
+  href: string;
+  icon: (props: { focused?: boolean; color: string }) => JSX.Element;
+  selected: boolean;
+  name: string;
+}) {
   const isLarge = useWidth(1265);
   // const buildLink = useLinkBuilder();
 
   // return null;
   // console.log("side bar:", buildLink("media"));
   return (
-    <Link
+    <TabLink
       name={name}
       href={href}
       accessibilityHasPopup="menu"
       style={{
+        paddingBottom: 35,
         paddingVertical: 4,
         display: "flex",
         width: "100%",
@@ -299,101 +302,63 @@ function SideBarTabItem({ children, href, icon, selected, name }) {
           </span>
         )}
       </div>
-    </Link>
+    </TabLink>
   );
 }
 
-function ProfileImage({ style, ...props }) {
-  return (
-    <Image
-      {...props}
-      alt="Evan Bacon"
-      source={{
-        uri: "https://pbs.twimg.com/profile_images/1452152950810234886/-1PK6cNp_bigger.jpg",
-        // uri: "https://miro.medium.com/fit/c/48/48/0*7hpwPqrKW-8i1C3u.jpg",
-      }}
-      style={[
-        {
-          backgroundColor: "#71767b",
-          width: 24,
-          height: 24,
-          borderRadius: 999,
-          boxShadow: "rgb(0 0 0 / 5%) 0px 0px 0px 1px inset",
-        },
-        style,
-      ]}
-    />
-  );
-}
-
-import { QualifiedSlot } from "expo-router/build/views/Layout";
-import { Tabs } from "../../components/tab-bar/BottomTabs";
-// import { createContext } from '@radix-ui/react-context';
-
-export default function App({ children }) {
+export default function App() {
   const isRowLayout = useWidth(600);
 
-  // if (!isRowLayout) {
   return (
     <>
       <GlobalHead />
-      <Tabs
-        customView={
-          isRowLayout ? (
-            <View style={{ flex: 1, flexDirection: "row" }}>
-              <SideBar />
-              <QualifiedSlot />
-            </View>
-          ) : null
-        }
-        screenOptions={{
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarActiveTintColor: "black",
-        }}
-      >
-        <Tabs.Screen
-          name="(index)"
-          options={{
-            title: "Feed",
-            tabBarIcon: makeIcon("home"),
+      <Navigator>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <SideBar />
+          <Slot />
+        </View>
+      </Navigator>
+      {false && (
+        <Tabs
+          customView={
+            isRowLayout ? (
+              <View style={{ flex: 1, flexDirection: "row" }}>
+                <SideBar />
+                <QualifiedSlot />
+              </View>
+            ) : null
+          }
+          screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarActiveTintColor: "black",
           }}
-        />
+        >
+          <Tabs.Screen
+            name="(index)"
+            options={{
+              title: "Feed",
+              tabBarIcon: makeIcon("home"),
+            }}
+          />
 
-        <Tabs.Screen
-          name="(media)"
-          options={{
-            title: "Media",
-            tabBarIcon: makeIcon("mic"),
-          }}
-        />
+          <Tabs.Screen
+            name="(media)"
+            options={{
+              title: "Media",
+              tabBarIcon: makeIcon("mic"),
+            }}
+          />
 
-        <Tabs.Screen
-          name="about"
-          options={{
-            title: "Evan Bacon",
-            tabBarIcon: makeIcon("person"),
-          }}
-        />
-      </Tabs>
-    </>
-  );
-  // }
-
-  return (
-    <>
-      <GlobalHead />
-      <View
-        style={[{ flex: 1 }, isRowLayout && { flexDirection: "row-reverse" }]}
-      >
-        {!isRowLayout && <CustomHeader />}
-        <Navigator router={TabRouter}>
-          <View style={{ flex: 1 }}>
-            <Slot />
-          </View>
-          {isRowLayout ? <SideBar /> : <CustomTabBar />}
-        </Navigator>
-      </View>
+          <Tabs.Screen
+            name="about"
+            options={{
+              title: "Evan Bacon",
+              tabBarIcon: makeIcon("person"),
+            }}
+          />
+        </Tabs>
+      )}
     </>
   );
 }
@@ -423,133 +388,3 @@ function GlobalHead() {
 }
 
 const DARK = "rgba(41, 41, 41, 1)";
-const GOLD = "#A8A8A8";
-
-function CustomTabBar() {
-  return (
-    <TabBar>
-      <a href="#">
-        <Icons.Home style={{ color: DARK }} />
-      </a>
-      <a href="#">
-        <Icons.Search style={{ color: GOLD }} />
-      </a>
-      <a href="#">
-        <Icons.Lists style={{ color: GOLD }} />
-      </a>
-      <a href="#">
-        <ProfileImage
-          style={{
-            width: 24,
-            height: 24,
-          }}
-        />
-      </a>
-    </TabBar>
-  );
-}
-
-function CustomHeader() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View
-      asChild
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-
-        zIndex: 100,
-        height: 56 + top,
-        paddingTop: top,
-
-        // FML -- Shadows in RN suck
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 2,
-        shadowOpacity: 0.15,
-        shadowColor: "black",
-
-        boxShadow: "0px -2px 10px rgb(0 0 0 / 15%)",
-        flexDirection: "row",
-        flex: 1,
-        paddingHorizontal: 24,
-        alignItems: "center",
-        justifyContent: "space-between",
-        backgroundColor: "white",
-      }}
-    >
-      <header>
-        <a href="#">
-          <div
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-              padding: 5,
-              borderWidth: 1,
-              borderRadius: 100,
-              borderColor: "rgba(230, 230, 230, 1)",
-            }}
-          >
-            <Icons.Notifications
-              style={{ width: 25, height: 25, color: GOLD }}
-            />
-          </div>
-        </a>
-      </header>
-    </View>
-  );
-}
-
-function TabBar({ children, style }) {
-  const { bottom } = useSafeAreaInsets();
-  const childrenArray = React.Children.toArray(children);
-
-  const newChildren = childrenArray.map((child, index) => {
-    return (
-      <View
-        key={String(index)}
-        asChild
-        center
-        style={{
-          display: "flex",
-          flex: 1,
-        }}
-      >
-        {child}
-      </View>
-    );
-  });
-
-  return (
-    <View
-      asChild
-      style={[
-        {
-          maxHeight: 56 + bottom,
-          height: 56 + bottom,
-
-          shadowOffset: { width: 0, height: -2 },
-          shadowRadius: 2,
-          shadowOpacity: 0.15,
-          shadowColor: "black",
-
-          boxShadow: "0px 2px 10px rgb(0 0 0 / 15%)",
-          flexDirection: "row",
-          flex: 1,
-          alignItems: "stretch",
-          backgroundColor: "white",
-
-          paddingBottom: bottom,
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-        },
-        style,
-      ]}
-    >
-      <nav>{newChildren}</nav>
-    </View>
-  );
-}
