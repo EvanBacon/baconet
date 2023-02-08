e53fb5ac55d2311f4e8a50a866751292d9daa5db
evanbacon <bacon@expo.io>
2023-02-08 17:11:58 -0600

    wip

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 import "./components/fetch";
-import "expo-router/entry";
+// import "expo-router/entry";
+
+import "@expo/metro-runtime";
+
+import { ExpoRoot } from "expo-router";
+import Head from "expo-router/head";
+
+import { renderRootComponent } from "expo-router/build/renderRootComponent";
+
+// We add this elsewhere for rendering
+const HeadProvider =
+  typeof window === "undefined" ? React.Fragment : Head.Provider;
+
+const ctx = require.context("./app");
+
+// Must be exported or Fast Refresh won't update the context >:[
+export function App(props) {
+  process.env.IS_APP_CLIP = props.isClip;
+  return (
+    <HeadProvider>
+      <ExpoRoot context={ctx} />
+    </HeadProvider>
+  );
+}
+
+renderRootComponent(App);
